38a99963f93ebbd767006ef2d50441dc1d6e9df6
Camilo Andrés Hernández Marin <camiloahm@gmail.com>
2018-07-08 22:04:10 -0500

    deployment files

--- src/app/store.service.ts
+++ src/app/store.service.ts
@@ -9,12 +9,14 @@ export class StoreService {
   constructor(private _http: HttpClient, ) {}
 
   getStores() {
-    const configUrl = 'http://localhost:8080/stores';
+    // TODO: This url should be move to a ENV Variable 
+    const configUrl = 'http://35.233.189.54:8080/stores';
     return this._http.get<StoreData[]>(configUrl);
   }
 
   getCloseStores(lat: number, lng: number) {
-    const configUrl = 'http://localhost:8080/stores/findCloseStores';
+    // TODO: This url should be move to a ENV Variable 
+    const configUrl = 'http://35.233.189.54:8080/stores/findCloseStores';
     const httpParams: HttpParams = new HttpParams()
       .append('latitude', String(lat))
       .append('longitude', String(lng));
